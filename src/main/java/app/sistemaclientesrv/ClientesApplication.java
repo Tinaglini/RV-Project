3b2275e462e1557aceadd9e642e0b83555afd4ff
Lucas <lucas@tinaglini.com>
2025-08-17 10:52:27 -0300

    Application

---
+++ src/main/java/app/sistemaclientesrv/ClientesApplication.java
@@ -0,0 +1,13 @@
+package app.sistemaclientesrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientesApplication.class, args);
+    }
+
+}
